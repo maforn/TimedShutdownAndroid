00948b9981c7f51af84a283b10e3b4b65c9d78d3
maforn <matteo.fornaini@studio.unibo.it>
2024-01-31 00:24:14 +0100

    Added automation support

---
+++ app/src/main/java/com/maforn/timedshutdown/AutoSupportActivity.java
@@ -0,0 +1,16 @@
+package com.maforn.timedshutdown;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AutoSupportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(getApplicationContext(), AccessibilitySupportService.class);
+        getApplicationContext().startService(intent);
+    }
+}
