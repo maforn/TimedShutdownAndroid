2a975efd4a5e0b0bee5bf3d208437d656c61657a
maforn <matteo.fornaini@studio.unibo.it>
2024-01-31 11:23:28 +0100

    Updated info and added French support

--- app/src/main/java/com/maforn/timedshutdown/AutomationSupportActivity.java
+++ app/src/main/java/com/maforn/timedshutdown/AutomationSupportActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AutoSupportActivity extends AppCompatActivity {
+public class AutomationSupportActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
